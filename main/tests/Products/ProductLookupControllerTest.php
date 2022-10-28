f859463cd9a6f1902b012c12d8615d7c91fa7da7
Marcin Lubiejewski <mlubiejewski@trans.eu>
2022-10-28 17:18:22 +0200

    show_recommendations_on_product_lookup

--- main/tests/Products/ProductLookupControllerTest.php
+++ main/tests/Products/ProductLookupControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tbd\Main\Tests\Products;
 
+use Tbd\Main\FeatureFlags\FeatureFlag;
 use Tbd\Main\Products\Product;
 use Tbd\Main\Products\ProductLookupController;
 use PHPUnit\Framework\TestCase;
@@ -11,8 +12,12 @@ use Tbd\Main\Products\ProductRepositoryInterface;
 
 class ProductLookupControllerTest extends TestCase
 {
-    public function testControllerReturnsValidResponse()
+    public function testControllerReturnsValidResponseWithRecommendationsDisabled()
     {
+        if(FeatureFlag::isEnabled('show_recommendations_on_product_lookup')){
+            $this->markTestSkipped("Flag show_recommendations_on_product_lookup is enabled");
+        }
+
         $request = new ServerRequest('GET', 'http://example.com/products/3');
         $request = $request->withAttribute("id", "3");
 
@@ -38,6 +43,46 @@ class ProductLookupControllerTest extends TestCase
         $this->assertEquals($output, (string) trim($response->getBody()));
     }
 
+    public function testControllerReturnsValidResponseWithRecommendationsEnabled()
+    {
+        if(!FeatureFlag::isEnabled('show_recommendations_on_product_lookup')){
+            $this->markTestSkipped("Flag show_recommendations_on_product_lookup is disabled");
+        }
+
+        $request = new ServerRequest('GET', 'http://example.com/products/3');
+        $request = $request->withAttribute("id", "3");
+
+        $product = new Product(3, 'test', 'description', 100);
+
+        $stub = $this->createMock(ProductRepositoryInterface::class);
+        $stub->method('findProduct')
+            ->will($this->returnValueMap([["3", $product]]));
+
+        $controller = new ProductLookupController($stub);
+
+        $recoStub = $this->createMock(RecommendationsServiceInterface::class);
+        $recoStub->method('getRecommendations')
+            ->will($this->returnValueMap([[3, [1]]]));
+
+        $controller->getDataProvider()->getImplementation()->setService($recoStub);
+
+        $response = $controller($request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+
+        $output='{
+    "name": "test",
+    "description": "description",
+    "price": 100.0,
+    "recommendations": [
+        1
+    ]
+}';
+        $this->assertEquals($output, (string) trim($response->getBody()));
+    }
+    
     public function testControllerReturns404Response()
     {
         $request = new ServerRequest('GET', 'http://example.com/products/3');
