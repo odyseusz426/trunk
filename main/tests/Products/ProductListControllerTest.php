9616a2c6a86419c3f29af93ab26fbeb05ab91868
Marcin Lubiejewski <mlubiejewski@trans.eu>
2022-10-28 16:17:32 +0200

    2. Testy automatyczne

--- main/tests/Products/ProductListControllerTest.php
+++ main/tests/Products/ProductListControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tbd\Main\Tests\Products;
 
+use Tbd\Main\FeatureFlags\FeatureFlag;
 use Tbd\Main\Products\Product;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
@@ -13,6 +14,10 @@ class ProductListControllerTest extends TestCase
 {
     public function testControllerReturnsValidResponseWithDetailsDisabled()
     {
+        if(FeatureFlag::isEnabled('show_product_details_on_list')){
+            $this->markTestSkipped("Flag show_product_details_on_list is enabled");
+        }
+
         $request = new ServerRequest('GET', 'http://example.com/products/');
 
         $product1 = new Product(1, 'test', 'description', 100);
@@ -60,6 +65,46 @@ class ProductListControllerTest extends TestCase
         $this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
 
         $output='[]';
+        $this->assertEquals($output, (string) trim($response->getBody()));
+    }
+
+    public function testControllerReturnsValidResponseWithDetailsEnabled()
+    {
+        if(!FeatureFlag::isEnabled('show_product_details_on_list')){
+            $this->markTestSkipped("Flag show_product_details_on_list is disabled");
+        }
+
+        $request = new ServerRequest('GET', 'http://example.com/products/');
+
+        $product1 = new Product(1, 'test', 'description', 100);
+        $product2 = new Product(2, 'test2', 'description2', 200);
+
+        $stub = $this->createMock(ProductRepositoryInterface::class);
+        $stub->method('listProducts')
+            ->willReturn([$product1, $product2]);
+
+        $controller = new ProductsListController($stub);
+
+        $response = $controller($request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+
+        $output='[
+    {
+        "id": 1,
+        "name": "test",
+        "description": "description",
+        "price": 100.0
+    },
+    {
+        "id": 2,
+        "name": "test2",
+        "description": "description2",
+        "price": 200.0
+    }
+]';
         $this->assertEquals($output, (string) trim($response->getBody()));
     }
 }
